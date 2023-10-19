3e48e1644b72a686b44638d2951e8799cef012ff
Chris <chl9430@gmail.com>
2023-10-19 17:47:56 +0900

    Changed folder structor. Added PlayerControls script into player ship.

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField] float controlSpeed = 10f;
+    void Update()
+    {
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
+
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
+        float newXPos = transform.localPosition.x + xOffset;
+
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+        float newYPos = transform.localPosition.y + yOffset;
+
+        transform.localPosition = new Vector3(
+            newXPos,
+            newYPos,
+            transform.localPosition.z
+            );
+    }
+}
